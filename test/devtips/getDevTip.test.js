ace3ab7e46290b7f0bbaff9997d2f4387b2a5afb
Pradumna saraf <pradumnasaraf@gmail.com>
2023-06-24 16:17:41 +0530

    update tests

--- test/devtips/getDevTip.test.js
+++ test/devtips/getDevTip.test.js
@@ -1,6 +1,6 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const server = require("../../index.js");
+import chai from "chai";
+import chaiHttp from "chai-http";
+import server from "../../index.js";
 
 // Assertion Style
 chai.should();
